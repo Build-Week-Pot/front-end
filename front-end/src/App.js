0f133188cb1418e0372c7fb5fd7211c5cc7cc4e0
[Tristan Harrower] <[harrowertristan@gmail.com]>
2021-10-18 18:54:56 -0400

    finished routing working on styling homepage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import { Link, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Form from './components/Form';
+
 
 
 
@@ -8,8 +14,26 @@ function App() {
   return (
     <div className="App">
       <Header  />
+      {
+            <Switch>
+                <Route path="/create">
+                    <Form />
+                </Route>
+                <Route path='/login'>
+                    <Login />
+                </Route>
+                <Route path='/register'>
+                    <Register />
+                </Route>
+                <Route path='/'>
+                  <Home />
+                </Route>
+            </Switch>
+        }
       <Footer />
     </div>
+
+    
   );
 }
 
--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,47 +1,31 @@
-import { Link, Route, Switch } from 'react-router-dom';
-import Login from './Login';
-import Register from './Register';
-import UserDashboard from './UserDashboard';
+import { Link } from 'react-router-dom';
+
 
 
 export default function Header(){
-/**
- * <nav>
-            <Link to="/"><h1>Potluck Planners</h1></Link> 
-            <Link to="/login"><h1>Login</h1></Link> 
-            <Link to="/register"><h1>Register</h1></Link> 
-        </nav>
- */
     return(
     <div>
-        <nav class="navbar navbar-expand navbar-light bg-light">
-            <Link to="/" class='navbar-brand'><h1>Potluck Planners</h1></Link> 
-            
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <Link to="/login" class="nav-link" href="#">Login <span class="sr-only">(current)</span></Link>
-                </li>
-                <li class="nav-item">
-                    <Link to="/register"class="nav-link" href="#">Register</Link>
-                </li>
-            </ul>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+                <Link to="/" class='navbar-brand'><h1>Potluck Planners</h1></Link> 
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <Link to="/login" class="nav-link" >Login <span class="sr-only">(current)</span></Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/register"class="nav-link" >Register</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/create"class="nav-link" >Create Potluck</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
+        
 
-
-
-        {
-            <Switch>
-                <Route path='/login'>
-                    <Login />
-                </Route>
-                <Route path='/register'>
-                    <Register />
-                </Route>
-                <Route path='/'>
-                    <UserDashboard/>
-                </Route>
-            </Switch>
-        }
     </div>
     )
 }
