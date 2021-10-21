fc50180c248fc4978ecd1b11dd0514040a773115
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2021-10-21 01:10:32 -0400

    fixed both register and login validation

--- front-end/src/Validations/LoginValidation.js
+++ front-end/src/Validations/LoginValidation.js
@@ -1,7 +1,9 @@
 import * as yup from "yup"
 
 
-export const loginSchema = yup.object().shape({
+ const loginSchema = yup.object().shape({
     username: yup.string().min(5, 'Please enter a valid username').required('Please enter a valid username'),
     password: yup.string().min(6, 'Please enter a valid password').required('Please enter a valid password'),
 })
+
+export default loginSchema;
--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -72,16 +72,12 @@ const setFormErrors = (name, value) => {
             
     }
 
-    const onSubmit =(e) => {
-        handleSubmit(e);
-        change(e);
-        handleChanges(e)
-        
-    }
+
 
     const errorSubmit = (e) => {
        change(e)
        handleChanges(e)
+       
 
     }
 
@@ -91,7 +87,7 @@ return (
         <div className="mt-5" style={{display: 'flex', justifyContent: 'center'}}>
         <div className="shadow p-5 mb-20 rounded  w-50 mt-1" style={{backgroundColor: '#8ecae6', border:'1px solid black'}}> 
             <h1 className="text-left pb-5" style={{fontWeight: '500'}}>Sign Up</h1>
-            <form className="text-left" onSubmit={onSubmit}>
+            <form className="text-left" onSubmit={handleSubmit}>
 
             <img style={{color: 'black', width: '3.4%', marginRight: '5px', paddingRight:'2px', paddingBottom:'4px'}} src={`${process.env.PUBLIC_URL}/assets/person.png`} alt="logo"/>
                 <input className="border-1 shadow"
@@ -209,7 +205,7 @@ return (
                 <input type="submit"
                 disabled={disabled}
                 className="shadow p-1 mb-5 bg-black rounded mt-4 m-4 m-5"
-                style={{width: '14%', height: '5vh', backgroundColor: 'black', color: 'white', borderRadius: '7%' }}/><br/>
+                style={{width: '14%', height: '5vh', backgroundColor: 'black', color: 'white', borderRadius: '7%'}}/><br/>
            
         <img style={{color: 'black', width: '3.8%', marginRight: '3px', paddingRight:'2px', paddingBottom:'4px', marginRight: '40px'}} src={`${process.env.PUBLIC_URL}/assets/fb.png`} alt="logo"/>
         <img style={{color: 'black', width: '3.4%', marginRight: '3px', paddingRight:'2px', paddingBottom:'4px', marginRight: '40px'}} src={`${process.env.PUBLIC_URL}/assets/google.png`} alt="logo"/>
